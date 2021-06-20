d2db3cc23442fa27738968b31de121ccfc4dffcb
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-06-20 10:19:11 +0700

    blog: move `Unmarshal` trait to encoding/bson

--- src/blog/mod.rs
+++ src/blog/mod.rs
@@ -3,8 +3,3 @@ use mongodb::bson::Document;
 pub mod service;
 pub mod post;
 pub mod taxonomy;
-
-trait Unmarshal {
-    fn unmarshal_bson(document: &Document) -> Result<Self,mongodb::bson::document::ValueAccessError> where Self: Sized;
-}
-
