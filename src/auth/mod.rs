eca93845a12b0054af789f8e2fb88d4545058685
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-06-29 22:13:12 +0700

    Remove debug code

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -1,13 +1,8 @@
-use std::convert::TryFrom;
-
 use alcoholic_jwt::{token_kid, validate, Validation, JWKS};
 use serde::{Deserialize, Serialize};
-use tonic::metadata::MetadataValue;
 use tonic::{Request, Status};
 
 pub mod user;
-
-pub const JWT_CLAIMS: &str = "claims";
 
 /// User context that deserializes from the JSON Web Token string.
 #[derive(Debug, Deserialize, Serialize)]
@@ -17,11 +12,11 @@ struct Claims {
 }
 
 /// The gRPC interceptor for validating and extracting user info from the Bearer token (if exists).
-pub fn interceptor(
+pub fn new_interceptor(
     authority: String,
     audience: String,
     jwks: JWKS,
-) -> impl Fn(Request<()>) -> Result<Request<()>, Status> {
+) -> impl FnMut(Request<()>) -> Result<Request<()>, Status> {
     move |mut r: Request<()>| -> Result<Request<()>, Status> {
         if let Some(metadata) = r.metadata().get("Authorization") {
             let validations = vec![
@@ -47,34 +42,8 @@ pub fn interceptor(
                 })
             {
                 if let Ok(valid_jwt) = result {
-                    // let claims: Claims = serde_json::from_value(valid_jwt.claims).unwrap();
-                    //
-                    // let metadta_value: MetadataValue<Claims> =
-                    //     MetadataValue::try_from(claims).unwrap();
-
                     r.extensions_mut()
                         .insert(serde_json::from_value::<Claims>(valid_jwt.claims).unwrap());
-                    // r.metadata_mut().insert(
-                    //     JWT_SUB,
-                    //     MetadataValue::from_str(
-                    //         valid_jwt.claims.get("sub").unwrap().as_str().unwrap(),
-                    //     )
-                    //     .unwrap(),
-                    // );
-
-                    // r.metadata_mut().insert(
-                    //     JWT_CLAIMS,
-                    //     MetadataValue::try_from(Claims::default()).unwrap(),
-                    // );
-                    // r.metadata_mut().insert("")
-                    // r.metadata_mut().insert(
-                    //     "claims",
-                    //     MetadataValue(valid_jwt.claims.get("sub").unwrap().as_str()),
-                    //     // Claims {
-                    //     //     sub: valid_jwt.claims.get("sub").unwrap().as_str().to_string(),
-                    //     //     ..Default::default()
-                    //     // },
-                    // );
 
                     return Ok(r);
                 }
