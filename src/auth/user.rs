433b4b82cf5aea8f18d1c74bdda6ce44405ec15a
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-07-03 15:26:07 +0700

    Rename service, update marshaler, un-marshaler trait name

--- src/auth/user.rs
+++ src/auth/user.rs
@@ -4,23 +4,27 @@ use mongodb::bson::Document;
 use myblog_proto_rust::myblog::proto::auth::User;
 use prost_types::Timestamp;
 
-use crate::encoding::bson::Unmarshal;
+use crate::encoding::bson::Unmarshaler;
 
-impl Unmarshal for User {
-    fn unmarshal_bson(document: &Document) -> Result<Self, mongodb::bson::document::ValueAccessError> where Self: Sized {
-        Ok(
-            User {
-                id: document.get_object_id("_id")?.to_hex(),
-                user: document.get_str("user")?.to_owned(),
-                display_name: document.get_str("displayName")?.to_owned(),
-                profile_picture: document.get_str("profilePicture")?.to_owned(),
-                created_at: Some(document.get_datetime("createdAt")
-                    .and_then(|created_at| Ok(Timestamp::from(SystemTime::from(created_at.to_owned()))))?),
-                updated_at: match document.get_datetime("updatedAt") {
-                    Ok(updated_at) => Some(Timestamp::from(SystemTime::from(updated_at.to_owned()))),
-                    _ => None,
-                },
-            }
-        )
+impl Unmarshaler for User {
+    fn unmarshal_bson(
+        document: &Document,
+    ) -> Result<Self, mongodb::bson::document::ValueAccessError>
+    where
+        Self: Sized,
+    {
+        Ok(User {
+            id: document.get_object_id("_id")?.to_hex(),
+            user: document.get_str("user")?.to_owned(),
+            display_name: document.get_str("displayName")?.to_owned(),
+            profile_picture: document.get_str("profilePicture")?.to_owned(),
+            created_at: Some(document.get_datetime("createdAt").and_then(|created_at| {
+                Ok(Timestamp::from(SystemTime::from(created_at.to_owned())))
+            })?),
+            updated_at: match document.get_datetime("updatedAt") {
+                Ok(updated_at) => Some(Timestamp::from(SystemTime::from(updated_at.to_owned()))),
+                _ => None,
+            },
+        })
     }
 }
--- src/storage/file.rs
+++ src/storage/file.rs
@@ -4,27 +4,35 @@ use mongodb::bson::Document;
 use myblog_proto_rust::myblog::proto::storage::File;
 use prost_types::Timestamp;
 
-use crate::encoding::bson::Unmarshal;
+use crate::encoding::bson::Unmarshaler;
 
-impl Unmarshal for File {
-    fn unmarshal_bson(document: &Document) -> Result<Self, mongodb::bson::document::ValueAccessError> where Self: Sized {
-        Ok(
-            File {
-                id: document.get_object_id("_id")?.to_hex(),
-                file_name: document.get_str("fileName")?.to_owned(),
-                slug: document.get_str("slug")?.to_owned(),
-                uploaded_file_path: document.get_str("uploadedFilePath")?.to_owned(),
-                mime_type: document.get_str("mimeType")?.to_owned(),
-                provider: document.get_str("provider")?.to_owned(),
-                region: document.get_str("region")?.to_owned(),
-                bucket: document.get_str("bucket")?.to_owned(),
-                uploaded_at: Some(document.get_datetime("uploadedAt")
-                    .and_then(|uploaded_at| Ok(Timestamp::from(SystemTime::from(uploaded_at.to_owned()))))?),
-                modified_at: match document.get_datetime("modifiedAt") {
-                    Ok(modified_at) => Some(Timestamp::from(SystemTime::from(modified_at.to_owned()))),
-                    _ => None,
-                },
-            }
-        )
+impl Unmarshaler for File {
+    fn unmarshal_bson(
+        document: &Document,
+    ) -> Result<Self, mongodb::bson::document::ValueAccessError>
+    where
+        Self: Sized,
+    {
+        Ok(File {
+            id: document.get_object_id("_id")?.to_hex(),
+            file_name: document.get_str("fileName")?.to_owned(),
+            slug: document.get_str("slug")?.to_owned(),
+            uploaded_file_path: document.get_str("uploadedFilePath")?.to_owned(),
+            mime_type: document.get_str("mimeType")?.to_owned(),
+            provider: document.get_str("provider")?.to_owned(),
+            region: document.get_str("region")?.to_owned(),
+            bucket: document.get_str("bucket")?.to_owned(),
+            uploaded_at: Some(
+                document
+                    .get_datetime("uploadedAt")
+                    .and_then(|uploaded_at| {
+                        Ok(Timestamp::from(SystemTime::from(uploaded_at.to_owned())))
+                    })?,
+            ),
+            modified_at: match document.get_datetime("modifiedAt") {
+                Ok(modified_at) => Some(Timestamp::from(SystemTime::from(modified_at.to_owned()))),
+                _ => None,
+            },
+        })
     }
 }
