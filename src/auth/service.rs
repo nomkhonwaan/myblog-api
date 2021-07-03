383eb9a32219348f1972eb05d10b1cb2689447f5
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-07-03 21:21:23 +0700

    (WIP) Scaffold auth service

--- src/auth/service.rs
+++ src/auth/service.rs
@@ -1,6 +1,41 @@
-use myblog_proto_rust::myblog::proto::auth::auth_service_server::AuthService;
+use myblog_proto_rust::myblog::proto::auth::{
+    auth_service_server::AuthService,
+    CreateUserRequest, CreateUserResponse,
+};
+use tonic::{Request, Response, Status};
 
-pub struct MyAuthService {}
+use crate::auth::user::UserRepository;
+
+pub struct MyAuthService {
+    user_repository: Box<dyn UserRepository>,
+}
+
+impl MyAuthService {
+    pub fn builder() -> MyAuthServiceBuilder { MyAuthServiceBuilder::default() }
+}
 
 #[tonic::async_trait]
-impl AuthService for MyAuthService {}
+impl AuthService for MyAuthService {
+    async fn create_user(&self, request: Request<CreateUserRequest>) -> Result<Response<CreateUserResponse>, Status> {
+        todo!()
+    }
+}
+
+#[derive(Default)]
+pub struct MyAuthServiceBuilder {
+    /* Repository */
+    user_repository: Option<Box<dyn UserRepository>>,
+}
+
+impl MyAuthServiceBuilder {
+    pub fn with_user_repository(mut self, repository: Box<dyn UserRepository>) -> Self {
+        self.user_repository = Some(repository);
+        self
+    }
+
+    pub fn build(self) -> MyAuthService {
+        MyAuthService {
+            user_repository: self.user_repository.unwrap(),
+        }
+    }
+}
