60eab9876747d8401c27233bb63bde4a1c34dd0e
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-07-05 11:50:29 +0700

    Implement marshal-ing function on comment struct

--- src/discussion/comment.rs
+++ src/discussion/comment.rs
@@ -1,17 +1,17 @@
 use std::str::FromStr;
 use std::time::SystemTime;
 
-use mongodb::{bson::doc, bson::Document, Collection};
+use mongodb::{bson::DateTime, bson::doc, bson::Document, bson::oid::ObjectId, Collection};
 use myblog_proto_rust::myblog::proto::auth::User;
 use myblog_proto_rust::myblog::proto::discussion::Comment;
 use prost_types::Timestamp;
-use tokio_stream::StreamExt;
 
 use crate::encoding::bson::{Marshaler, Unmarshaler};
 
+// A comment repository definition.
 #[tonic::async_trait]
 pub trait CommentRepository: Send + Sync + 'static {
-    async fn create(&self, c: Comment);
+    async fn create(&self, c: &mut Comment) -> Result<(), Box<dyn std::error::Error>>;
 }
 
 /// An implementation of the CommentRepository specifies with MongoDB.
@@ -27,33 +27,54 @@ impl MongoCommentRepository {
 
 #[tonic::async_trait]
 impl CommentRepository for MongoCommentRepository {
-    async fn create(&self, c: Comment) {
-        // mongodb::bson::DateTime::from_millis() c.created_at.unwrap().seconds
-        // self.collection.insert_one()
+    async fn create(&self, c: &mut Comment) -> Result<(), Box<dyn std::error::Error>> {
+        if c.id.is_empty() {
+            c.id = ObjectId::new().to_hex();
+        }
+
+        self.collection.insert_one(&c.marshal_bson()?, None).await?;
+
+        Ok(())
     }
 }
 
-// impl Marshaler for Comment {
-//     fn marshal_bson(&self) -> Document {
-//         let mut document = doc! {
-//             "status": self.status,
-//             "text": self.text.to_string(),
-//             // "author": self.author.marshal_bson(),
-//             // "parent": self.parent.marshal_bson(),
-//             // "children": self.children.marshal_bson(),
-//             "created_at": mongodb::bson::DateTime
-//         };
-//
-//         document
-//     }
-// }
+impl Marshaler for Comment {
+    fn marshal_bson(&self) -> Result<Document, mongodb::bson::oid::Error> {
+        let mut document = doc! {
+            "_id": ObjectId::from_str(self.id.as_str())?,
+            "status": self.status,
+            "text": self.status,
+            "author": ObjectId::from_str(self.author.as_ref().unwrap().id.as_str())?,
+            "children": self.children
+                .iter()
+                .map(|c| ObjectId::from_str(c.id.as_str()))
+                .collect::<Result<Vec<ObjectId>, _>>()?,
+            "createdAt": DateTime::from_millis(self.created_at.as_ref().unwrap().seconds * 1000),
+        };
+
+        if self.parent.is_some() {
+            document.insert(
+                "parent",
+                ObjectId::from_str(self.parent.as_ref().unwrap().id.as_str())?,
+            );
+        }
+        if self.updated_at.is_some() {
+            document.insert(
+                "updatedAt",
+                DateTime::from_millis(self.updated_at.as_ref().unwrap().seconds * 1000),
+            );
+        }
+
+        Ok(document)
+    }
+}
 
 impl Unmarshaler for Comment {
     fn unmarshal_bson(
         document: &Document,
     ) -> Result<Self, mongodb::bson::document::ValueAccessError>
-    where
-        Self: Sized,
+        where
+            Self: Sized,
     {
         Ok(Comment {
             id: document.get_object_id("_id")?.to_hex(),
