7db5f9a172510d1a351e9bb49f0a1964282024cc
Natcha Luangaroonchai <me@nomkhonwaan.com>
2021-07-12 00:42:56 +0700

    Use envsubst for better replacing string

--- scripts/create-comment.sh
+++ scripts/create-comment.sh
@@ -2,10 +2,17 @@
 
 set -e
 
+DATE=$(date +'%Y-%m-%d')
+TIME=$(date +'%H:%m:%S')
+export DATETIME="${DATE}T${TIME}.000Z"
+
+TEMPLATE='{"comment":{"text":"Hello, world!","createdAt":"${DATETIME}","status":"Published"}}'
+BODY=$(echo $TEMPLATE | envsubst)
+
 grpcurl -plaintext \
   -import-path ../myblog-proto \
   -proto proto/discussion/service.proto \
-  -d '{"comment":{"text":"Hello, world!","createdAt":"2020-07-14T01:47:53.188Z","status":"Published"}}' \
+  -d "${BODY}" \
   -H "Authorization: Bearer ${ACCESS_TOKEN}" \
   localhost:8080 \
   myblog.proto.discussion.DiscussionService/CreateComment
