296b0cdff343f04253847d7382fdca678b595f0c
Natcha Luangaroonchai <me@nomkhonwaan.com>
2022-03-05 10:43:48 +0700

    Update category ID

--- scripts/list-category-published-posts.sh
+++ scripts/list-category-published-posts.sh
@@ -5,6 +5,6 @@ set -e
 grpcurl -plaintext \
   -import-path ../myblog-proto \
   -proto proto/blog/service.proto \
-  -d '{"category":{"id": "5f0d384fbb5a7bb644623cb2"}, "offset": 0, "limit": 1}' \
+  -d '{"category":{"id": "5b2863365c31b411b041995e"}, "offset": 0, "limit": 1}' \
   localhost:8082 \
   myblog.proto.blog.BlogService/ListCategoryPublishedPosts
