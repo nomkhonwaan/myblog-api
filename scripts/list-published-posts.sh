96711a66cdf647a20b4af931ac971817d3f2e5b0
Natcha Luangaroonchai <me@nomkhonwaan.com>
2022-03-05 09:17:10 +0700

    Change default blog-service port from 8080 to 8082

--- scripts/list-published-posts.sh
+++ scripts/list-published-posts.sh
@@ -6,5 +6,5 @@ grpcurl -plaintext \
   -import-path ../myblog-proto \
   -proto proto/blog/service.proto \
   -d '{"offset": 0, "limit": 1}' \
-  localhost:8080 \
+  localhost:8082 \
   myblog.proto.blog.BlogService/ListPublishedPosts
